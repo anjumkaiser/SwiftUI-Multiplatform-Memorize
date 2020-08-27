7efcd47f8e63575f43c828f2e2c1243585913f95
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-08-27 08:07:32 -0700

    Added Rows and Columns of cards

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            ForEach(0..<4) { vindex in
+                HStack {
+                    ForEach(0..<4) { index in
+                        CardView(isFacedUp: false)
+                    }
+                }
+            }
+        }
+        
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFacedUp: Bool
+    
+    var body: some View {
+        if isFacedUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 10.0).fill()
+        }
     }
 }
 
