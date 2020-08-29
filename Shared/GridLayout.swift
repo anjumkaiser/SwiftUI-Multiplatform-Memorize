a46611377b9edb39492afa067313c966e21fbd83
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-08-29 06:24:49 -0700

    better progres with GridLayout

--- Shared/GridLayout.swift
+++ Shared/GridLayout.swift
@@ -18,7 +18,7 @@ struct GridLayout {
     init (itemCount: Int, nearAspectRatio desiredAspectRatio: Double = 1, in size: CGSize) {
         self.size = size
         rowCount = 3
-        columnCount = 3
+        columnCount = 2
         itemSize = CGSize()
         itemSize.width = CGFloat(self.size.width) / CGFloat(columnCount) * CGFloat(desiredAspectRatio)
         itemSize.height = CGFloat(self.size.height) / CGFloat(rowCount) * CGFloat(desiredAspectRatio)
@@ -28,24 +28,24 @@ struct GridLayout {
     func location(ofItemAt index: Int) -> CGPoint {
         
         let padding = 10
-        let initialX = 50
+        let initialX = 75
         let initialY = 90
         
         var x: CGFloat = CGFloat(initialX)
         var y: CGFloat = CGFloat(initialY)
         
-        if index < 3 {
+        if index < 2 {
             x += (CGFloat (itemSize.width) * CGFloat(index))
-        } else if index < 6 {
-            x += (CGFloat (itemSize.width) * CGFloat(index - 3))
+        } else if index < 4 {
+            x += (CGFloat (itemSize.width) * CGFloat(index - 2))
             y += (CGFloat (itemSize.height))
-        } else if index < 9 {
-            x += (CGFloat (itemSize.width) * CGFloat(index - 6))
+        } else if index < 6 {
+            x += (CGFloat (itemSize.width) * CGFloat(index - 4))
             y += (CGFloat (itemSize.height) * 2)
         }
         
         x += CGFloat(padding)
-        y += CGFloat( padding)
+        y += CGFloat(padding)
   
         let point = CGPoint(x: x, y: y)
         return point
