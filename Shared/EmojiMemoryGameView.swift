254aec9e5bc8a00897c0425e1503e8465d3af8b9
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-11-02 19:43:59 +0500

    added a small transition to Pie appear

--- Shared/EmojiMemoryGameView.swift
+++ Shared/EmojiMemoryGameView.swift
@@ -64,6 +64,7 @@ struct CardView: View {
                     }
                 }
                 .padding(5).opacity(0.4)
+                .transition(.scale)
                 
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
